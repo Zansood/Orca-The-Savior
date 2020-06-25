73d5801682fa3a555c45ab938815eeabc1f7094d
Zansood <60878775+Zansood@users.noreply.github.com>
2020-06-25 07:04:17 +0700

    Add files via upload

---
+++ TrashSystem.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+
+public class TrashSystem : MonoBehaviour
+{
+    public Gamedata data;
+
+    public Slider sliderHealthPlayer;
+    public Slider sliderHungryPlayer;
+    public Text txtTrashcount;
+    static public int trashCount;
+    public Button trash;
+    [SerializeField] float longDistance;
+    [SerializeField] Transform player;
+    float distanceToPlayer = Mathf.Infinity;
+
+    void Update()
+    {
+        checkPlayer();
+    }
+ 
+    public void checkPlayer()
+    {
+        distanceToPlayer = Vector3.Distance(player.position, transform.position);
+        if (distanceToPlayer <= longDistance)
+        {
+            sliderHealthPlayer.value -= 0.001f;
+            trash.gameObject.SetActive(true);
+        }
+        else
+        {
+            trash.gameObject.SetActive(false);
+        }
+    }
+       
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, longDistance);
+    }
+   
+    public void destroytrash()
+    {
+        NewBehaviourScript.trashCount += 1;
+        txtTrashcount.text = NewBehaviourScript.trashCount.ToString();
+        sliderHungryPlayer.value -= 0.2f;
+        gameObject.SetActive(false);
+        //Destroy(other.gameObject); //game crash!!!
+        trash.gameObject.SetActive(false);
+    }
+    
+}
